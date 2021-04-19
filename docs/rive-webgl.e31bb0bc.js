399b04628ab7c57d8d7956feb3dd150ac4bd26c9
Luigi Rosso <luigi.rosso@gmail.com>
2021-04-18 20:01:32 -0700

    Updating build.

--- docs/rive-webgl.e31bb0bc.js
+++ docs/rive-webgl.e31bb0bc.js
@@ -21464,7 +21464,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63062" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52959" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
